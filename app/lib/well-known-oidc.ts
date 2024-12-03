7b9279026c4180ef89e1b2fdaa37ac55b5a1e16a
Troplo <troplo@troplo.com>
2024-12-03 23:15:17 +1100

    Fix well known openid configuration

--- app/lib/well-known-oidc.ts
+++ app/lib/well-known-oidc.ts
@@ -1,14 +1,14 @@
 function getWellKnownOidc() {
   const apiPrefix =
     config.hostnameWithProtocol === "https://flowinity.com"
-      ? "https://api.flowinity.com/v3/"
-      : `${config.hostnameWithProtocol}/api/v3/`
+      ? "https://api.flowinity.com/v3"
+      : `${config.hostnameWithProtocol}/api/v3`
   return {
     issuer: `${config.hostnameWithProtocol}`,
     authorization_endpoint: `${config.hostnameWithProtocol}/oauth`,
-    token_endpoint: `${apiPrefix}/v3/oidc/token`,
-    userinfo_endpoint: `${apiPrefix}/v3/oidc/userinfo`,
-    jwks_uri: `${apiPrefix}/v3/oidc/jwks.json`,
+    token_endpoint: `${apiPrefix}/oidc/token`,
+    userinfo_endpoint: `${apiPrefix}/oidc/userinfo`,
+    jwks_uri: `${apiPrefix}/oidc/jwks.json`,
     scopes_supported: ["openid", "profile", "email", "offline_access"],
     response_types_supported: ["code", "id_token", "token id_token"],
     token_endpoint_auth_methods_supported: ["client_secret_basic"],
